cc04b46e7fcf718d9dc34e87f7760522b9a81003
wadhah101 <wadhah.mahroug15@gmail.com>
2021-08-05 17:32:10 +0100

    expaned pipeline to root dir

--- lambdas/mail/status-mail.ts
+++ lambdas/mail/status-mail.ts
@@ -4,16 +4,28 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import AWS from "aws-sdk";
+
+const params = {
+  restApiId: "STRING_VALUE" /* required */,
+  deploymentId: "STRING_VALUE",
+};
 
 export const handler = async (
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
   const result = { event, context };
+  const apiGateway = new AWS.APIGateway({ params });
+
+  const stages = await apiGateway.getStages().promise();
+
+  console.log(stages);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      result,
+      stages,
     }),
   };
 };
--- readme.md
+++
@@ -1,4 +0,0 @@
-# CFN-lint-tets-pipeline
-
-A pipeline defined by cloudformation integrating cfn-lint , cfn-nag & taskcat
-The pipeline code exists in /pipeline and the rest is just for testing things
