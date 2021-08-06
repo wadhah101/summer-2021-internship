0fca3b0904a50ef64a5f27b0099ecb7dc58e28f1
wadhah101 <wadhah.mahroug15@gmail.com>
2021-08-06 09:33:42 +0100

    added new source for multi repo build

--- create.sh
+++ create.sh
@@ -1,1 +1,5 @@
-aws cloudformation create-stack --stack-name cfn-lint-test-pipelineV9 --capabilities CAPABILITY_NAMED_IAM --disable-rollback   --template-body file://pipeline/cfn.pipeline.yml
+# aws cloudformation create-stack --stack-name cfn-lint-test-pipelineV9 --capabilities CAPABILITY_NAMED_IAM --disable-rollback   --template-body file://pipeline/cfn.pipeline.yml
+
+
+
+sam deploy --guided
--- update.sh
+++ update.sh
@@ -1,1 +1,4 @@
-aws cloudformation update-stack --stack-name cfn-lint-test-pipelineV9 --capabilities CAPABILITY_NAMED_IAM   --template-body file://pipeline/cfn.pipeline.yml
+# aws cloudformation update-stack --stack-name cfn-lint-test-pipelineV9 --capabilities CAPABILITY_NAMED_IAM   --template-body file://pipeline/cfn.pipeline.yml
+
+
+sam build && sam deploy 
