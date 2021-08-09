55e6febfa2d902035d5d1e77ed47d7f7073dff0e
wadhah101 <wadhah.mahroug15@gmail.com>
2021-08-09 18:20:30 +0100

    remove redundant files

--- create.sh
+++
@@ -1,5 +0,0 @@
-# aws cloudformation create-stack --stack-name cfn-lint-test-pipelineV9 --capabilities CAPABILITY_NAMED_IAM --disable-rollback   --template-body file://pipeline/cfn.pipeline.yml
-
-
-
-sam deploy --guided
--- update.sh
+++
@@ -1,4 +0,0 @@
-# aws cloudformation update-stack --stack-name cfn-lint-test-pipelineV9 --capabilities CAPABILITY_NAMED_IAM   --template-body file://pipeline/cfn.pipeline.yml
-
-
-sam build && sam deploy 
