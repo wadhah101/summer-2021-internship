a1b49c7bbc2c046f2811386cb5e151b937606267
wadhah101 <wadhah.mahroug15@gmail.com>
2021-08-09 12:57:12 +0100

    deploy to s3 with action

--- lambdas/mail/approve-mail.ts
+++ lambdas/mail/approve-mail.ts
@@ -1,15 +1,11 @@
 import "source-map-support/register";
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  Context,
-} from "aws-lambda";
+import { APIGatewayProxyEvent, SNSEvent, Context } from "aws-lambda";
 
 export const handler = async (
-  event: APIGatewayProxyEvent,
+  event: SNSEvent,
   context: Context
 ): Promise<void> => {
-  const result = { event, context };
-  console.log(result);
+  const pipelineResult = JSON.parse(event.Records[0].Sns.Message);
+  console.log(pipelineResult);
   return;
 };
