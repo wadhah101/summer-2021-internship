937040911ebaa352cf34d2262524b5495a3f79c9
wadhah101 <wadhah.mahroug15@gmail.com>
2021-08-18 12:32:24 +0100

    added dynamodb pipeline function

--- serverless/src/database/addPipelineResult.ts
+++ serverless/src/database/addPipelineResult.ts
@@ -1,65 +1,86 @@
 import { join } from "path";
 import "source-map-support/register";
-import { DynamoDB, S3 } from "aws-sdk";
+import { DynamoDB, S3, CodePipeline } from "aws-sdk";
 import { FullCFNResult } from "../@types/FullCFNResult";
+import { CodePipelineEvent, Context } from "aws-lambda";
+import unzipper from "unzipper";
+import { Readable } from "stream";
 
-interface MyEventRule {
-  version: string;
-  id: string;
-  "detail-type": string;
-  source: string;
-  account: string;
-  time: Date;
-  region: string;
-  resources: string[];
-  detail: Detail;
-}
-
-interface Detail {
-  pipeline: string;
-  "execution-id": string;
-  stage: string;
-  state: string;
-  version: number;
-}
-
-const tableName = process.env.TABLE_NAME;
+const { TABLE_NAME } = process.env;
 const db = new DynamoDB.DocumentClient();
-const bucketName = process.env.BUCKET_NAME;
+const codepipeline = new CodePipeline();
 const s3 = new S3();
 
-export const handler = async (event: MyEventRule): Promise<void> => {
-  if (!tableName) throw new Error("tableName not defined");
-  if (!bucketName) throw new Error("bucketName is not defined");
-  const executionID = event.detail["execution-id"];
+const streamToString = (stream: Readable): Promise<string> => {
+  const chunks: Buffer[] = [];
+  return new Promise((resolve, reject) => {
+    stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on("error", (err) => reject(err));
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+  });
+};
 
-  const lintObjPromise = s3
-    .getObject({ Bucket: bucketName, Key: join(executionID, "lint.json") })
-    .promise()
-    .then((e) => e.Body?.toString());
+export const handler = async (
+  event: CodePipelineEvent,
+  context: Context
+): Promise<void> => {
+  if (!TABLE_NAME) throw new Error("TABLE_NAME not defined");
 
-  const nagObjPromise = s3
-    .getObject({ Bucket: bucketName, Key: join(executionID, "nag.json") })
-    .promise()
-    .then((e) => e.Body?.toString());
+  const inputArtifacts = event["CodePipeline.job"].data.inputArtifacts;
+  const jobId = event["CodePipeline.job"].id;
+  const putJobSuccess = () =>
+    codepipeline.putJobSuccessResult({ jobId }).promise();
+  const postJobFailure = (failureDetails: CodePipeline.FailureDetails) =>
+    codepipeline.putJobFailureResult({ jobId, failureDetails });
 
-  const [lintStr, nagStr] = await Promise.all([lintObjPromise, nagObjPromise]);
-  if (!lintStr) throw new Error("lint file not found");
-  if (!nagStr) throw new Error("nag file not found");
+  try {
+    const lintingArt = inputArtifacts.find(
+      (e) => e.name === "LintingArtifacts"
+    );
+    if (!lintingArt) throw new Error("linting artifact not defined");
 
-  const [CFNLintResult, CFNNagResult] = [
-    JSON.parse(lintStr),
-    JSON.parse(nagStr),
-  ];
+    const nagArt = inputArtifacts.find((e) => e.name === "NagArtifacts");
+    if (!nagArt) throw new Error("nag artifact not defined");
 
-  const Item: FullCFNResult = {
-    executionID,
-    CFNLintResult,
-    date: new Date().toISOString(),
-    CFNNagResult,
-  };
+    const executionID = event["CodePipeline.job"].id;
 
-  const res = await db.put({ TableName: tableName, Item }).promise();
+    const lintZipStream = s3
+      .getObject({
+        Bucket: lintingArt.location.s3Location.bucketName,
+        Key: lintingArt.location.s3Location.objectKey,
+      })
+      .createReadStream()
+      .pipe(unzipper.ParseOne(/lint\.json/));
+    const nagStream = s3
+      .getObject({
+        Bucket: nagArt.location.s3Location.bucketName,
+        Key: nagArt.location.s3Location.objectKey,
+      })
+      .createReadStream()
+      .pipe(unzipper.ParseOne(/nag\.json/));
 
-  console.log(JSON.stringify(res));
+    const [lint, nag] = await Promise.all([
+      streamToString(nagStream),
+      streamToString(lintZipStream),
+    ]);
+
+    const [CFNLintResult, CFNNagResult] = [JSON.parse(lint), JSON.parse(nag)];
+
+    const Item: FullCFNResult = {
+      executionID,
+      CFNLintResult,
+      date: new Date().toISOString(),
+      CFNNagResult,
+    };
+
+    console.log(Item);
+
+    const res = await db.put({ TableName: TABLE_NAME, Item }).promise();
+
+    console.log(JSON.stringify(res));
+
+    await putJobSuccess();
+  } catch (e: unknown) {
+    await postJobFailure({ message: String(e), type: "JobFailed" });
+  }
 };
